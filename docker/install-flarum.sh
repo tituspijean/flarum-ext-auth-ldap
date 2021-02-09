4a101c49e1610731abfe9ab63de77001b6eb492d
CodazziS <stephane@codazzi.fr>
2021-02-09 22:41:20 +0100

    improve installation

--- docker/install-flarum.sh
+++ docker/install-flarum.sh
@@ -2,9 +2,9 @@ rm -rf /var/www/flarum
 mkdir /var/www/flarum
 cd /var/www/flarum || exit
 echo "Installing Flarum ..."
-composer create-project flarum/flarum . --stability=dev
+composer create-project --stability=dev flarum/flarum .
+composer config minimum-stability dev
 
 echo "Installing LDAP extension ..."
-composer require tituspijean/flarum-ext-auth-ldap "dev-master"
-rm -rf /var/www/flarum/vendor/tituspijean/flarum-ext-auth-ldap
-ln -s /var/www/ /var/www/flarum/vendor/tituspijean/flarum-ext-auth-ldap
+composer config repositories.repo-name path /var/www
+composer require tituspijean/flarum-ext-auth-ldap
