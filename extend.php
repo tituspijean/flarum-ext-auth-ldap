8b926f482251b5a7fd951b95de7b071e12e40b23
tituspijean <tituspijean@outlook.com>
2021-04-28 23:01:40 +0200

    Support for Flarum beta 16

--- extend.php
+++ extend.php
@@ -5,16 +5,16 @@ use Flarum\Foundation\Application;
 use Illuminate\Events\Dispatcher;
 
 return [
+  (new Extend\Locales(__DIR__ . '/locale')),
+  (new Extend\Frontend('admin'))
+    ->js(__DIR__.'/js/dist/admin.js'),
   (new Extend\Frontend('forum'))
     ->js(__DIR__.'/js/dist/forum.js')
     ->css(__DIR__.'/less/forum.less'),
-  (new Extend\Frontend('admin'))
-    ->js(__DIR__.'/js/dist/admin.js'),
-  (new Extend\Locales(__DIR__ . '/locale')),
   (new Extend\Routes('forum'))
     ->post('/auth/ldap', 'auth.ldap', Controllers\LDAPAuthController::class)
     ->get('/auth/ldap', 'auth.ldap', Controllers\LDAPAuthController::class),
-  function (Dispatcher $events, Application $app) {
-      $events->subscribe(Listeners\AddApiAttributes::class);
-  },
+  (new Extend\Settings)
+    ->serializeToForum('onlyUseLDAP', 'tituspijean-auth-ldap.onlyUse')
+    ->serializeToForum('LDAP_method_name', 'tituspijean-auth-ldap.method_name'),
 ];
