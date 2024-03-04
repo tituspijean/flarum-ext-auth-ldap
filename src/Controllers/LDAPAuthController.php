6b8d4ac1a36bf2f0bb0623d1731c909f0f3f87b2
Michael Yip <michael@poyu.co.uk>
2024-03-04 13:22:11 +0000

    Implement multiple domains settings

--- src/Controllers/LDAPAuthController.php
+++ src/Controllers/LDAPAuthController.php
@@ -34,112 +34,148 @@ class LDAPAuthController implements RequestHandlerInterface
 		$params = Arr::only($body, ['identification', 'password']);
 		$id = Arr::get($params, 'identification');
 		$password = Arr::get($params, 'password');
-		$config = [
-			'hosts' => explode(',', $this->settings->get($settingsPrefix . 'hosts')),
-			'username' => $this->settings->get($settingsPrefix . 'admin_dn'),
-			'password' => $this->settings->get($settingsPrefix . 'admin_password'),
-			'port' => intval($this->settings->get($settingsPrefix . 'port')),
-			'follow_referrals' => boolval($this->settings->get($settingsPrefix . 'follow_referrals')),
-			'use_ssl' => boolval($this->settings->get($settingsPrefix . 'use_ssl')),
-			'use_tls' => boolval($this->settings->get($settingsPrefix . 'use_tls')),
-			'timeout' => 5
-		];
-		$ldapErrors = [];
-		$userLdapUsername = $this->settings->get($settingsPrefix . 'user_username');
+
+		return $this->processDomains($id, $password);
+	}
+
+	public function processDomains(string $id, string $password): ResponseInterface
+	{
 		if (empty($id) || empty($password)) {
 			$contents = [
 				"errors" => array(["status" => 401, "code" => "account.invalid_inputs"]),
 			];
 			return new JsonResponse($contents, 401);
-		} else if (empty($userLdapUsername)) {
+		}
+		$domains = json_decode($this->settings->get('tituspijean-auth-ldap.domains'), true);
+
+		$domainsCount = $domains? count($domains): 0;
+		if ($domainsCount == 0) {
 			$contents = [
-				"errors" => array(["status" => 401, "code" => "username_is_invalid"]),
+				"errors" => array(["status" => 401, "code" => "domains.no_domains"]),
 			];
 			return new JsonResponse($contents, 401);
-		} else {
-			// For use with Nicknames extension enabled
-			$searchNicknameFields = $this->settings->get($settingsPrefix . 'search_nickname_fields');
+		} 
+		$ldapErrors = [];
+		for ($index = 0; $index < $domainsCount; $index++) {
+			$domain = $domains[$index];
+			if (empty($domain['host'])) {
+				$contents = [
+					"errors" => array(["status" => 401, "code" => "domains.empty_host", "domain_index" => $index+1])
+				];
+				return new JsonResponse($contents, 401);
+			}
+			$config = [
+				'hosts' => explode(',', $domain['host']),
+				'username' => $domain['admin']['dn'],
+				'password' => $domain['admin']['password'],
+				'port' => intval($domain['port']),
+				'follow_referrals' => boolval($domain['followReferrals']),
+				'use_ssl' => boolval($domain['useSSL']),
+				'use_tls' => boolval($domain['useTLS']),
+				'timeout' => 5
+			];
+			$userLdapUsername = $domain['user']['username'];
+			if (empty($userLdapUsername)) {
+				$contents = [
+					"errors" => array(["status" => 401, "code" => "domains.empty_user_username", "domain_index" => $index+1]),
+				];
+				return new JsonResponse($contents, 401);
+			} else {
+				// For use with Nicknames extension enabled
+				$searchNicknameFields = $domain['user']['nicknames'];
+	
+				$searchBaseDNs = $domain['baseDN'];
+				if (empty($searchBaseDNs)) {
+					$contents = [
+						"errors" => array(["status" => 401, "code" => "domains.empty_base_dn", "domain_index" => $index+1]),
+					];
+					return new JsonResponse($contents, 401);
+				} 
+				$filter = $domain['filter'];
+				$searchUserFields = $domain['searchFields'];
+				if (empty($searchUserFields)) {
+					$contents = [
+						"errors" => array(["status" => 401, "code" => "domains.empty_search_field", "domain_index" => $index+1]),
+					];
+					return new JsonResponse($contents, 401);
+				} 
+				$userLdapMail = $domain['user']['mail'];
 
-			$searchBaseDNs = $this->settings->get($settingsPrefix . 'base_dn');
-			$filter = $this->settings->get($settingsPrefix . 'filter');
-			$searchUserFields = $this->settings->get($settingsPrefix . 'search_user_fields');
-			$userLdapMail = $this->settings->get($settingsPrefix . 'user_mail');
+				$connection = new Connection($config);
 
-			$connection = new Connection($config);
+				foreach (explode(';', $searchBaseDNs) as $searchBaseDN) {
+					foreach ($searchUserFields as $searchUserField) {
+						try {
 
-			foreach (explode(';', $searchBaseDNs) as $searchBaseDN) {
-				foreach (explode(',', $searchUserFields) as $searchUserField) {
-
-					try {
-
-						if (!isset($filter) || $filter != '') {
-							$user = $connection->query()
-								->setDn($searchBaseDN)
-								->where($searchUserField, '=', $id)
-								->rawFilter($filter)
-								->firstOrFail();
-						} else {
-							$user = $connection->query()
-								->setDn($searchBaseDN)
-								->where($searchUserField, '=', $id)
-								->firstOrFail();
-						}
-						if ($connection->auth()->attempt($user['dn'], $password, $bindAsUser = true)) {
-							if (!array_key_exists(strtolower($userLdapUsername), $user)) {
-								// Prevent response showing exception for null username field
-								$contents = [
-									"errors" => array(["status" => 401, "code" => "username_field_does_not_exist", "data" => $userLdapUsername]),
-								];
-								return new JsonResponse($contents, 401);
+							if (!isset($filter) || $filter != '') {
+								$user = $connection->query()
+									->setDn($searchBaseDN)
+									->where($searchUserField, '=', $id)
+									->rawFilter($filter)
+									->firstOrFail();
+							} else {
+								$user = $connection->query()
+									->setDn($searchBaseDN)
+									->where($searchUserField, '=', $id)
+									->firstOrFail();
 							}
-							if (!empty($userLdapMail) && !array_key_exists(strtolower($userLdapMail), $user)) {
-								$contents = [
-									"errors" => array(["status" => 401, "code" => "mail_field_does_not_exist", "data" => $userLdapMail]),
-								];
-								return new JsonResponse($contents, 401);
-							}
-							return $this->response->make(
-								'ldap',
-								$user[strtolower($userLdapUsername)][0],
-								function (Registration $registration) use ($user, $userLdapUsername, $userLdapMail, $searchNicknameFields) {
-									$foundNickname = [];
-									foreach (explode(',', $searchNicknameFields) as $searchNicknameField) {
-										if (array_key_exists(strtolower($searchNicknameField), $user)) {
-											array_push($foundNickname, $user[strtolower($searchNicknameField)][0]);
+							if ($connection->auth()->attempt($user['dn'], $password, $bindAsUser = true)) {
+								if (!array_key_exists(strtolower($userLdapUsername), $user)) {
+									// Prevent response showing exception for null username field
+									$contents = [
+										"errors" => array(["status" => 401, "code" => "domains.username_field_does_not_exist", "data" => $userLdapUsername], "domain_index" => $index+1),
+									];
+									return new JsonResponse($contents, 401);
+								}
+								if (!empty($userLdapMail) && !array_key_exists(strtolower($userLdapMail), $user)) {
+									$contents = [
+										"errors" => array(["status" => 401, "code" => "domains.mail_field_does_not_exist", "data" => $userLdapMail, "domain_index" => $index+1]),
+									];
+									return new JsonResponse($contents, 401);
+								}
+								return $this->response->make(
+									'ldap',
+									$user[strtolower($userLdapUsername)][0],
+									function (Registration $registration) use ($user, $userLdapUsername, $userLdapMail, $searchNicknameFields) {
+										$foundNickname = [];
+										foreach ($searchNicknameFields as $searchNicknameField) {
+											if (array_key_exists(strtolower($searchNicknameField), $user)) {
+												array_push($foundNickname, $user[strtolower($searchNicknameField)][0]);
+											}
+										}
+										$registration
+											->suggest('isLDAP', true)
+											->provide('username', $user[strtolower($userLdapUsername)][0])
+											//->provideAvatar($user->getJpegPhoto())
+											->setPayload((array)$user['dn']);
+										if (count($foundNickname) > 0) {
+											$registration
+												->provide('nickname', implode(" ", $foundNickname));
+										}
+										if (!empty($userLdapMail)) {
+											$registration
+												->provideTrustedEmail($user[strtolower($userLdapMail)][0]);
 										}
 									}
-									$registration
-										->suggest('isLDAP', true)
-										->provide('username', $user[strtolower($userLdapUsername)][0])
-										//->provideAvatar($user->getJpegPhoto())
-										->setPayload((array)$user['dn']);
-									if (count($foundNickname) > 0) {
-										$registration
-											->provide('nickname', implode(" ", $foundNickname));
-									}
-									if (!empty($userLdapMail)) {
-										$registration
-											->provideTrustedEmail($user[strtolower($userLdapMail)][0]);
-									}
+								);
+							} else {
+								$error = $connection->getLdapConnection()->getDiagnosticMessage();
+	
+								if (strpos($error, '532') !== false) {
+									throw new Exception("account.password_expired");
+								} elseif (strpos($error, '533') !== false) {
+									throw new Exception("account.disabled");
+								} elseif (strpos($error, '701') !== false) {
+									throw new Exception("account.expired");
+								} elseif (strpos($error, '775') !== false) {
+									throw new Exception("account.locked");
 								}
-							);
-						} else {
-							$error = $connection->getLdapConnection()->getDiagnosticMessage();
-
-							if (strpos($error, '532') !== false) {
-								throw new Exception("account.password_expired");
-							} elseif (strpos($error, '533') !== false) {
-								throw new Exception("account.disabled");
-							} elseif (strpos($error, '701') !== false) {
-								throw new Exception("account.expired");
-							} elseif (strpos($error, '775') !== false) {
-								throw new Exception("account.locked");
+	
+								throw new Exception("account.incorrect_details");
 							}
-
-							throw new Exception("account.incorrect_details");
+						} catch (Exception $e) {
+							$ldapErrors[] = $e->getMessage();
 						}
-					} catch (Exception $e) {
-						$ldapErrors[] = $e->getMessage();
 					}
 				}
 			}
